b0909cc21b8c35cce3236cad8201c85124d1d533
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2024-02-11 14:35:48 +0500

    routing login signup cart page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+import CartPage from './pages/CartPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />,
+  },
+  {
+    path: '/cart',
+    element: <CartPage />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
